04f4ed3fac0428583ac98cd5776a07c4ab457067
sup3rbi3l <135216652+sup3rbi3l@users.noreply.github.com>
2025-05-19 08:12:34 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -109,7 +109,6 @@ def get_video_info_from_youtube(video_url):
         video_id = extract_video_id_py(video_url)
         
         ydl_opts = {
-            'cookiefile': 'cookies.txt',
             'quiet': True,  
             'skip_download': True,
         }   
@@ -246,7 +245,7 @@ def download_ig_video():
             print('720p')
             ext = 'mp4'
             ydl_opts = {
-                'cookiefile': 'cookies.txt',
+
                 'format': 'bestvideo[height<=720][ext=mp4]+bestaudio/best',
                 'merge_output_format': 'mp4',
                 'outtmpl': '%(title)s.%(ext)s',
@@ -257,7 +256,7 @@ def download_ig_video():
             print('360p')
             ext = 'mp4'
             ydl_opts = {
-                'cookiefile': 'cookies.txt',
+
                 'format': 'bestvideo[height<=360][ext=mp4]+bestaudio/best',
                 'merge_output_format': 'mp4',
                 'outtmpl': '%(title)s.%(ext)s',
@@ -268,7 +267,7 @@ def download_ig_video():
             print('mp3')
             ext = 'mp3'
             ydl_opts = {
-                'cookiefile': 'cookies.txt',
+
                 'format': 'bestaudio/best',
                 'quiet': False,
                 'postprocessors': [{
